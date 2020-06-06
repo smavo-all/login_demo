d7cb5ed12ce116531de2978ebcf785a69193748f
Sergio Martin Alexander Villagomez Ortega <martin.v.ortega@gmail.com>
2020-06-06 02:08:17 -0500

    Validacion del Formulario

--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UsuarioModel } from '../../models/usuario.models';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -8,7 +9,7 @@ import { UsuarioModel } from '../../models/usuario.models';
 })
 export class RegistroComponent implements OnInit {
 
-  usuario:UsuarioModel;
+  usuario: UsuarioModel;
 
   constructor() { }
 
@@ -16,13 +17,16 @@ export class RegistroComponent implements OnInit {
     this.usuario = new UsuarioModel();
 
     this.usuario.email = 'martin.v.ortega@gmail.com';
-    this.usuario.nombre = 'Sergio Villagomez';
-    this.usuario.password = '123456'
+    /*this.usuario.nombre = 'Sergio Villagomez';
+    this.usuario.password = '123456'*/
   }
 
-  onSubmit(){
+  onSubmit(form: NgForm) {
+    if (form.invalid) { return; }
+
     console.log('formulario enviado');
     console.log(this.usuario);
+    console.log(form);
   }
 
 }
