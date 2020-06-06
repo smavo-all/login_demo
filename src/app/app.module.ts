fb4fb1317837ff4d7cac21cb35156a9fce61d775
Sergio Martin Alexander Villagomez Ortega <martin.v.ortega@gmail.com>
2020-06-06 00:12:25 -0500

    add Component Home, Login, Registro, add assets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegistroComponent } from './pages/registro/registro.component';
