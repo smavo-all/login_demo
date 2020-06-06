fce5cc332af64598ea0de1b0dacfa783827fa35a
Sergio Martin Alexander Villagomez Ortega <martin.v.ortega@gmail.com>
2020-06-06 03:04:41 -0500

    add services/auth, Firebase [private url, apikey]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import {FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,7 +21,8 @@ import { RegistroComponent } from './pages/registro/registro.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { UsuarioModel } from '../models/usuario.models';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+private url = 'https://identitytoolkit.googleapis.com/v1'
+private apikey = 'AIzaSyDp3rie0YbxZ4bXAPozJxoasU475J9Lj0A'
+
+  // Sign up with email / password
+  //https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]
+
+
+  // Sign in with email / password
+  //https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=[API_KEY]
+
+  constructor( private http:HttpClient) { 
+  }
+
+  logout(){
+
+  }
+
+  login(usaurio: UsuarioModel){
+
+  }
+
+  nuevoUsuario( usuario: UsuarioModel){
+
+  }
+
+
+  
+}
