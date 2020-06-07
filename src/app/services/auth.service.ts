d2a22b2ebc8c11bb717cc727ae7f9dd6bebb9761
Sergio Martin Alexander Villagomez Ortega <martin.v.ortega@gmail.com>
2020-06-06 21:39:54 -0500

    Guardando Token en el Local Storage

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
+
 import { HttpClient } from '@angular/common/http';
+
 import { UsuarioModel } from '../models/usuario.models';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,13 +12,7 @@ export class AuthService {
 
   private url = 'https://identitytoolkit.googleapis.com/v1'
   private apikey = 'AIzaSyDp3rie0YbxZ4bXAPozJxoasU475J9Lj0A'
-
-  // Sign up with email / password - 06/06/2020
-  //https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]
-
-
-  // Sign in with email / password 06/06/2020
-  //https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=[API_KEY]
+  userToken: string;
 
   constructor(private http: HttpClient) {
   }
@@ -24,6 +21,8 @@ export class AuthService {
   }
 
   login(usuario: UsuarioModel) {
+    // Sign up with email / password - 06/06/2020
+    //https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]
 
     const authData = {
       ...usuario,
@@ -33,11 +32,18 @@ export class AuthService {
     return this.http.post(
       `${this.url}/accounts:signInWithPassword?key=${this.apikey}`,
       authData
+    ).pipe(
+      map(resp => {
+        console.log('Mapa RXJS');
+        this.guardarToken(resp['idToken']);
+        return resp;
+      })
     );
-
   }
 
   nuevoUsuario(usuario: UsuarioModel) {
+    // Sign in with email / password 06/06/2020
+    //https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=[API_KEY]
 
     const authData = {
       /*email: usuario.email,
@@ -49,8 +55,33 @@ export class AuthService {
     return this.http.post(
       `${this.url}/accounts:signUp?key=${this.apikey}`,
       authData
+    ).pipe(
+      map(resp => {
+        console.log('Mapa RXJS');
+        this.guardarToken(resp['idToken']);
+        return resp;
+      })
     );
 
   }
+
+  private guardarToken(idToken: string) {
+
+    this.userToken = idToken;
+    localStorage.setItem('token', idToken);
+  }
+
+  leerToken() {
+
+    if (localStorage.getItem('token')) {
+      this.userToken = localStorage.getItem('token');
+    } else {
+      this.userToken = '';
+    }
+    return this.userToken;
+
+  }
+
+
 
 }
